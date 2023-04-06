4f0667e2aaaf504db4a83eb2fadb344512aeaa60
Pavel Pozdnyakov <pvplpt@inbox.ru>
2023-04-06 22:38:47 +0500

    добавил решение задачи на C#

--- Program.cs
+++ Program.cs
@@ -13,3 +13,43 @@
 // 
 //=============================================================================
 
+
+Console.Clear();
+string[] userArray = { "Hello", "2", "world", ":-)" };
+
+string[] newArray = GetArrayStringLengthLE3(userArray);
+
+Console.WriteLine($"[{String.Join(", ", userArray)}] -> [{String.Join(", ", newArray)}]");
+
+
+// Функция копирования n элементов arr в новый массив
+string[] CopyArray(string[] arr, int n)
+{
+    if (n == arr.Length) return arr;
+    else
+    {
+        string[] result = new string[n];
+        for (int i = 0; i < n; i++)
+        {
+            result[i] = arr[i];
+        }
+        return result;
+    }
+}
+
+
+// Функция возвращает новый массив из строк 
+// длина которых меньше, либо равна 3 символам
+string[] GetArrayStringLengthLE3(string[] arr)
+{
+    int length = arr.Length;
+    string[] result = new string[length];
+    int count = 0;
+    for (int i = 0; i < length; i++)
+        if (arr[i].Length <= 3)
+        {
+            result[count] = arr[i];
+            count++;
+        }
+    return CopyArray(result, count);
+}
