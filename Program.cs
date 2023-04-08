7951e5aab3d5e65117b99f23148d6edc7504ad0d
Pavel Pozdnyakov <pvplpt@inbox.ru>
2023-04-08 23:37:51 +0500

    добавил ввод массива

--- Program.cs
+++ Program.cs
@@ -17,13 +17,13 @@
 Console.Clear();
 
 //input
-string[] userArray = { "Hello", "2", "world", ":-)" };
+string[] userArray = InputArray();
 
 //logic
 string[] newArray = GetArrayStringLengthLE3(userArray);
 
 //output
-Console.WriteLine($"{ArrayToString(userArray)} -> {ArrayToString(newArray)}");
+Console.WriteLine($"\n{ArrayToString(userArray)} -> {ArrayToString(newArray)}");
 
 
 // Функция копирования n элементов arr в новый массив
@@ -62,6 +62,57 @@ static string[] GetArrayStringLengthLE3(string[] arr)
 
 
 // функция представляет массив строк в виде одной строки
-static string ArrayToString(string[] arr) {
+static string ArrayToString(string[] arr)
+{
+    if (arr.Length == 0) return "[]";
     return $"[\"{String.Join("\", \"", arr)}\"]";
+}
+
+
+//Функция ввода натурального числа
+static int InputNaturalNumber(string msg, int defaultValue)
+{
+    int num;
+    Console.Write(msg);
+    if (int.TryParse(Console.ReadLine(), out num))
+    {
+        if (num <= 0) num = defaultValue;
+    }
+    else
+    {
+        num = defaultValue;
+    }
+    return num;
+}
+
+
+// Функция ввода массива строк
+static string[] InputArray()
+{
+    //Веведем варианты готовых массивов
+    Console.WriteLine("1. [\"Hello\", \"2\", \"world\", \":-)\"]");
+    Console.WriteLine("2. [\"1234\", \"1567\", \"-2\", \"computer science\"]");
+    Console.WriteLine("3. [\"Russia\", \"Denmark\", \"Kazan\"]");
+    Console.WriteLine("4. [\"RUB\", \"EUR\", \"USD\"]");
+    Console.WriteLine("5. [\"Bye\"]");
+    Console.WriteLine("6. []");
+    Console.WriteLine("7. Ввод пользователя\n");
+
+    int userChoice = InputNaturalNumber("Ваш выбор (по умолчанию 1): ", 1);
+
+    if (userChoice == 1) return new[] { "Hello", "2", "world", ":-)" };
+    if (userChoice == 2) return new[] { "1234", "1567", "-2", "computer science" };
+    if (userChoice == 3) return new[] { "Russia", "Denmark", "Kazan"};
+    if (userChoice == 4) return new[] { "RUB", "EUR", "USD"};
+    if (userChoice == 5) return new[] { "Bye" };
+    if (userChoice == 6) return new string[0];
+
+    int size = InputNaturalNumber("Введите длину массива (по умолчанию 3): ", 3);
+    string[] result = new string[size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"Введите {i + 1} элемент массива: ");
+        result[i] = Console.ReadLine() ?? "";
+    }
+    return result;
 }
